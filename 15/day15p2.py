29e172a177b5614e4426771f20388d782e760bec
gaplin <gaplinn@gmail.com>
2024-01-05 17:06:38 +0100

    15

---
+++ 15/day15p2.py
@@ -0,0 +1,151 @@
+from queue import Queue
+from copy import deepcopy
+
+input = map(int, open('input2.txt').read().split(','))
+
+mem = [0 for _ in range(3000)]
+for idx, value in enumerate(input):
+    mem[idx] = value
+
+def get_modes(mask: int, count: int) -> list:
+    result = []
+    for _ in range(count):
+        result.append(mask % 10)
+        mask //= 10
+
+    return result
+
+def get_value(mem: list, mode: int, addr: int, offset: int) -> int:
+    if mode == 0:
+        return mem[mem[addr]]
+    elif mode == 1:
+        return mem[addr]
+    elif mode == 2:
+        return mem[mem[addr] + offset]
+    else:
+        raise Exception('invalid read mode: {}'.format(mode))
+
+def save_value(mem: list, mode: int, addr: int, offset: int, value: int):
+    if mode == 0:
+        mem[mem[addr]] = value
+    elif mode == 2:
+        mem[mem[addr] + offset] = value
+    else:
+        raise Exception('invalid write mode: {}'.format(mode))
+
+def simulate(program: list, input_value: int) -> int:
+    mem, ip, offset = program
+    output_value = None
+    while True:
+        operation = mem[ip]
+        opcode = operation % 100
+        if opcode == 99:
+            break
+        operation //= 100
+        if opcode == 1: # add
+            modes = get_modes(operation, 3)
+            result = get_value(mem, modes[0], ip + 1, offset) + get_value(mem, modes[1], ip + 2, offset)
+            save_value(mem, modes[2], ip + 3, offset, result)
+            ip += 4
+        elif opcode == 2: # mult
+            modes = get_modes(operation, 3)
+            result = get_value(mem, modes[0], ip + 1, offset) * get_value(mem, modes[1], ip + 2, offset)
+            save_value(mem, modes[2], ip + 3, offset, result)
+            ip += 4
+        elif opcode == 3: # input
+            modes = get_modes(operation, 1)
+            save_value(mem, modes[0], ip + 1, offset, input_value)
+            ip += 2
+        elif opcode == 4: # output
+            modes = get_modes(operation, 1)
+            output_value = get_value(mem, modes[0], ip + 1, offset)
+            ip += 2
+            break
+        elif opcode == 5: # JNZERO
+            modes = get_modes(operation, 2)
+            if get_value(mem, modes[0], ip + 1, offset) != 0:
+                ip = get_value(mem, modes[1], ip + 2, offset)
+            else:
+                ip += 3
+        elif opcode == 6: #  JZERO
+            modes = get_modes(operation, 2)
+            if get_value(mem, modes[0], ip + 1, offset) == 0:
+                ip = get_value(mem, modes[1], ip + 2, offset)
+            else:
+                ip += 3
+        elif opcode == 7: # LESS
+            modes = get_modes(operation, 3)
+            result = 0
+            if get_value(mem, modes[0], ip + 1, offset) < get_value(mem, modes[1], ip + 2, offset):
+                result = 1
+            save_value(mem, modes[2], ip + 3, offset, result)
+            ip += 4
+        elif opcode == 8: # Equal
+            modes = get_modes(operation, 3)
+            result = 0
+            if get_value(mem, modes[0], ip + 1, offset) == get_value(mem, modes[1], ip + 2, offset):
+                result = 1
+            save_value(mem, modes[2], ip + 3, offset, result)
+            ip += 4
+        elif opcode == 9: # Offset modification
+            modes = get_modes(operation, 1)
+            offset += get_value(mem, modes[0], ip + 1, offset)
+            ip += 2
+        else:
+            ip += 1
+
+    program[1] = ip
+    program[2] = offset
+    return output_value
+
+
+def get_all_tiles(mem: list) -> (set, tuple):
+    position = (0, 0)
+    Q = Queue()
+    program = [list(mem), 0, 0]
+    Q.put((position, program, 0))
+    visited = {position}
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    directions_map = {(-1, 0): 1, (1, 0): 2, (0, -1): 3, (0, 1): 4}
+    oxygen_position = None
+    all_nodes = set()
+
+    while Q.empty() == False:
+        position, program, distance = Q.get()
+        all_nodes.add(position)
+        for di, dii in directions:
+            new_position = (position[0] + di, position[1] + dii)
+            if new_position in visited:
+                continue
+            visited.add(new_position)
+            program_copy = deepcopy(program)
+            robot_dir = directions_map[(di, dii)]
+            tile_type = simulate(program_copy, robot_dir)
+            new_distance = distance + 1
+            if tile_type == 2:
+                oxygen_position = new_position
+            if tile_type >= 1:
+                Q.put((new_position, program_copy, new_distance))
+
+    return (all_nodes, oxygen_position)
+    
+def get_longest_path(start, all_nodes):
+    Q = Queue()
+    Q.put((start, 0))
+    visited = {start}
+    result = 0
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    while Q.empty() == False:
+        position, distance = Q.get()
+        result = distance
+        for di, dii in directions:
+            new_position = (position[0] + di, position[1] + dii)
+            if new_position not in visited and new_position in all_nodes:
+                visited.add(new_position)
+                Q.put((new_position, distance + 1))
+
+    return result
+
+all_nodes, oxygen_position = get_all_tiles(mem)
+result = get_longest_path(oxygen_position, all_nodes)
+print(result)
